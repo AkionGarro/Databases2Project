bf57c521386760645055bd3f7bf7c9744909c391
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-03-29 23:52:18 -0600

    App

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CsvReader from "./CsvReader";
+function App() {
+  return (
+    <div className="App">
+      <CsvReader />
+    </div>
+  );
+}
+
+export default App;
